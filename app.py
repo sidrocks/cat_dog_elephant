e1a8f70903868e20efb7c5459e3d6260cbf37e50
sidrocks <sidharthghag@gmail.com>
2025-08-24 21:44:54 +0530

    bracket not closed

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 @app.route("/upload", methods=["POST"])
 def upload_file():
